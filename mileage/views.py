956eae9309ae869ea0b45c93e0692d949d306676
Chris Ortman <chrisortman@gmail.com>
2016-10-28 13:35:05 +0000

    Render a template

--- mileage/views.py
+++ mileage/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-   return HttpResponse("Hello") 
+    some_text = """
+    Hello!
+    
+    This is a formatted
+    
+        text block
+    """
+    context = {'name' : "Chris", 'tt' : some_text}
+    return render(request, 'mileage/index.html', context) 
