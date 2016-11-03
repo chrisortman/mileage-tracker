3a6e2d3a445364a14aeb3fbb7037083d6b368069
Chris Ortman <chrisortman@gmail.com>
2016-11-03 13:45:54 +0000

    Form examples

--- mileage/apps.py
+++ mileage/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class MileageConfig(AppConfig):
     name = 'mileage'
+
+
--- mileage/views.py
+++ mileage/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
@@ -10,5 +11,22 @@ def index(request):
     
         text block
     """
-    context = {'name' : "Chris", 'tt' : some_text}
+    
+    
+    name = User.objects.first().username
+    context = {
+        'name' : name,
+        'tt' : some_text,
+        'birthday': '12/6/1979',
+        'numbers' : [1,2,3]
+    }
+    
     return render(request, 'mileage/index.html', context) 
+
+def other(request):
+    context = {}
+    return render(request, 'mileage/other.html', context) 
+
+def poop(request):
+    what_i_typed = request.POST['charlieTheChooChoo']
+    return HttpResponse("You entered <hr>%s<hr> into the box" % what_i_typed)
